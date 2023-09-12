912495c7ba3c6451e3974502acfd7a935702eb49
David Hanks <davidkhanks@gmail.com>
2023-09-12 15:21:51 -0700

    Move nvim-tree keybinds to it's own after file
    Move the nvim-tree bindings to the appropriate place instead of the generic remap file
    
    PORK: Add alternate key binding for opening Harpoon menu

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -3,6 +3,8 @@ local ui = require("harpoon.ui")
 
 vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<leader>bb", ui.toggle_quick_menu)
+-- alternate keybinding to open Harpoon
+vim.keymap.set("n", "<C-b>", ui.toggle_quick_menu)
 
 vim.keymap.set("n", "<C-j>", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<C-k>", function() ui.nav_file(2) end)
