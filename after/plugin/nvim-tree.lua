277045ba93d6425301ae2f35231857b5b2e89aac
David Hanks <davidkhanks@gmail.com>
2023-09-12 13:13:32 -0700

    Add git settings to nvim-tree
    Add some settings to allow for exclusions to the nvim-tree filter as well as a custom pattern to ignore the .git directory.

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -8,6 +8,14 @@ vim.opt.termguicolors = true
 -- pass to setup along with your other options
 require("nvim-tree").setup({
     auto_close = true,
+    filters = {
+        git_ignored = true,
+        dotfiles = false,
+        git_clean = false,
+        no_buffer = false,
+        custom = { "^.git$" },
+        exclude = { "local.py", ".devhome" },
+    },
     git = {
         show_on_open_dirs = false,
     },
